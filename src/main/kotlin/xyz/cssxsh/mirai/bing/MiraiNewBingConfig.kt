8a620377b40324efd750c04dd7ed9ab089b286d0
筱傑 <jie65535@qq.com>
2023-03-21 09:43:20 +0800

    Move newTopicGreet to config Rename chat_expiration_s to chat_expires_in

--- src/main/kotlin/xyz/cssxsh/mirai/bing/MiraiNewBingConfig.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/bing/MiraiNewBingConfig.kt
@@ -62,9 +62,25 @@ public object MiraiNewBingConfig : ReadOnlyPluginConfig("bing"), NewBingConfig {
     @ValueName("reset_prefix")
     public val reset: String by value("bing-reset")
 
-    @ValueName("chat_expiration_s")
+    @ValueName("chat_expires_in")
     public val expiration: Long by value(300L)
 
     @ValueName("show_source_attributions")
     public val source: Boolean by value(true)
+
+    @ValueName("new_topic_greet")
+    public val newTopicGreet: Array<String> by value(arrayOf(
+        "谢谢你帮我理清头绪! 我现在能帮你做什么?",
+        "谢谢你! 知道你什么时候准备好继续前进总是很有帮助的。我现在能为你回答什么问题?",
+        "重新开始总是很棒。问我任何问题!",
+        "当然，我很乐意重新开始。我现在可以为你提供哪些帮助?",
+        "好了，我已经为新的对话重置了我的大脑。你现在想聊些什么?",
+        "没问题，很高兴你喜欢上一次对话。让我们转到一个新主题。你想要了解有关哪些内容的详细信息?",
+        "当然，我已准备好进行新的挑战。我现在可以为你做什么?",
+        "好的，我已清理好板子，可以重新开始了。我可以帮助你探索什么?",
+        "明白了，我已经抹去了过去，专注于现在。我们现在应该探索什么?",
+        "很好，让我们来更改主题。你在想什么?",
+        "好了，我已经为新的对话擦拭干净板子了。现在我可以和你聊些什么呢?",
+        "不用担心，我很高兴尝试一些新内容。我现在可以为你回答什么问题?",
+    ))
 }
